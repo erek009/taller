df466fb1b33b14798dd5d03c763e5cc98be2dd5c
erek009 <eeriic.prz@gmail.com>
2025-03-17 20:25:39 -0600

    eliminar modelo

--- controller/ctrAnoVehiculo.php
+++ controller/ctrAnoVehiculo.php
@@ -6,7 +6,7 @@ require_once("../models/mdlAnoVehiculo.php");
 /* TODO: Inicializando clases */
 $AnoVehiculo = new mdlAnoVehiculo();
 
-switch ($_GET["option"]) {
+switch ($_GET["op"]) {
         /*TODO: Guardar y editar, guarda cuando el ID esta vacio y Actualiza cuando se envie el ID*/
     case "guardaryeditar":
         if (empty($_POST["token"])) {
@@ -31,15 +31,17 @@ switch ($_GET["option"]) {
         /*TODO: Listado de registros formato JSON para Datatable JS*/
     case "listar":
         $tabla = "ano";
-        $item = "token";
-        $valor = $_POST["token"];
-        $datos = $AnoVehiculo->mdlSeleccionarRegistros($tabla, $item, $valor);
+        // $item = "id";
+        // $valor = $_POST["token"];
+        $datos = $AnoVehiculo->mdlSeleccionarRegistros($tabla, null, null);
         $data = array();
+
         foreach ($datos as $row) {
             $sub_array = array();
             $sub_array[] = $row["ano"];
-            $sub_array[] = '<button type="button" onClick="editar(' . $row["TOKEN"] . ')" id="' . $row["TOKEN"] . '" class="btn btn-warning btn-icon waves-effect waves-light"><i class="ri-edit-2-line"></i></button>';
-            $sub_array[] = '<button type="button" onClick="eliminar(' . $row["TOKEN"] . ')" id="' . $row["TOKEN"] . '" class="btn btn-danger btn-icon waves-effect waves-light"><i class="ri-delete-bin-5-line"></i></button>';
+             $sub_array[] = $row["token"];
+            // $sub_array[] = '<button type="button" onClick="editar(' . $row["token"] . ')" id="' . $row["token"] . '" class="btn btn-warning btn-icon waves-effect waves-light"><i class="ri-edit-2-line"></i></button>';
+            $sub_array[] = '<button type="button" onClick="eliminar(' . $row["token"] . ')" id="' . $row["token"] . '" class="btn btn-danger btn-icon waves-effect waves-light"><i class="ri-delete-bin-5-line"></i></button>';
             $data[] = $sub_array;
         }
         $results = array(
---
+++ view/MntAno/mntano.js
@@ -0,0 +1,86 @@
+// console.log("test");
+
+
+$(document).ready(function () {
+    /* TODO: Listar informacion en el datatable js */
+    $("#table_data").DataTable({
+      aProcessing: true,
+      aServerSide: true,
+      dom: "Bfrtip",
+      buttons: ["copyHtml5", "excelHtml5", "csvHtml5"],
+      ajax: {
+        url: "../../controller/ctrAnoVehiculo.php?op=listar",
+        type: "post",
+        data: { token : 1},
+      },
+      bDestroy: true,
+      responsive: true,
+      bInfo: true,
+      iDisplayLength: 10,
+      order: [[0, "desc"]],
+      language: {
+        sProcessing: "Procesando...",
+        sLengthMenu: "Mostrar _MENU_ registros",
+        sZeroRecords: "No se encontraron resultados",
+        sEmptyTable: "Ningún dato disponible en esta tabla",
+        sInfo:
+          "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        sInfoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+        sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+        sInfoPostFix: "",
+        sSearch: "Buscar:",
+        sUrl: "",
+        sInfoThousands: ",",
+        sLoadingRecords: "Cargando...",
+        oPaginate: {
+          sFirst: "Primero",
+          sLast: "Último",
+          sNext: "Siguiente",
+          sPrevious: "Anterior",
+        },
+        oAria: {
+          sSortAscending:
+            ": Activar para ordenar la columna de manera ascendente",
+          sSortDescending:
+            ": Activar para ordenar la columna de manera descendente",
+        },
+      },
+    });
+  });
+
+
+  function eliminar(token) {
+
+    console.log(token);
+    exit;
+
+    swal
+      .fire({
+        title: "Eliminar!",
+        text: "Desea Eliminar el Registro?",
+        icon: "error",
+        confirmButtonText: "Si",
+        showCancelButton: true,
+        cancelButtonText: "No",
+      })
+      .then((result) => {
+        if (result.value) {
+          $.post(
+            "../../controller/ctrAnoVehiculo.php?op=eliminar",
+            { token: token },
+            function (data) {
+              console.log(data);
+            }
+          );
+  
+          $("#table_data").DataTable().ajax.reload();
+  
+          swal.fire({
+            title: "Año vehiculo",
+            text: "Registro Eliminado",
+            icon: "success",
+          });
+        }
+      });
+  }
+  
